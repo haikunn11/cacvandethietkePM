edb4d9fcc665b3813dde42cea8e910c7cd623ee0
haikunn11 <phamhaicute9@gmail.com>
2025-06-13 22:55:39 +0700

    Update CashService.java

--- 20_2251172334_PhamDinhHai_Bai6/adapter/CashService.java
+++ 20_2251172334_PhamDinhHai_Bai6/adapter/CashService.java
@@ -2,6 +2,6 @@ package adapter;
 
 public class CashService {
     public boolean CashPaymentProcessor(double amount) {
-        return amount > 0;
+       System.out.println("Cash: " +amount);
     }
 }
