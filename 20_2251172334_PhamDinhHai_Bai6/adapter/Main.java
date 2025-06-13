480a382eed6ec9276ab5ffa7c42b631ae1bed51f
haikunn11 <phamhaicute9@gmail.com>
2025-06-13 23:03:33 +0700

    Update Main.java

--- 20_2251172334_PhamDinhHai_Bai6/adapter/Main.java
+++ 20_2251172334_PhamDinhHai_Bai6/adapter/Main.java
@@ -2,8 +2,8 @@ package adapter;
 
 public class Main {
     public static void main(String[] args) {
-        PaymentProcess System1 = new CreditCardAdapter(new CreditCardService());
-        PaymentProcess System2 = new CashAdapter(new CashService());
+        PaymentSystem System1 = new CreditCardAdapter(new CreditCardService());
+        PaymentSystem System2 = new CashAdapter(new CashService());
 
         System1.ProcessPayment(500); 
         System2.ProcessPayment(200); 
